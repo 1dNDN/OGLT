7c43f8888531095ed962ceb3002b66510d3e5b2e
1dNDN <idndn@inbox.ru>
2022-04-08 16:22:22 +0300

    add basic shaders

--- OGLT/Program.cs
+++ OGLT/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+
+using RustedWarfareClient;
+
+namespace OGLT;
+
+internal static class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello World!");
+    }
+
+    private static void Init()
+    {
+        NativeWindowSettings nativeWindowSettings = new()
+        {
+            Size = new Vector2i(800, 600),
+            Title = "RW Client",
+            Flags = ContextFlags.ForwardCompatible
+        };
+
+        using Vent window = new(GameWindowSettings.Default, nativeWindowSettings);
+        window.Run();
+    }
+}
